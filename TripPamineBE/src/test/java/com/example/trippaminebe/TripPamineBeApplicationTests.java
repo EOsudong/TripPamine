8a95f11770762580e85df1ae99ccd006ea75f819
EOsudong <sttr8285@naver.com>
2026-08-05 12:53:39 +0900

    First commit

---
+++ TripPamineBE/src/test/java/com/example/trippaminebe/TripPamineBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trippaminebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPamineBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
