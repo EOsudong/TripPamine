ea10b8a8ed3ee94f17bece3108eeff50df2e4189
어수동 <sttr8285@naver.com>
2026-08-27 10:22:46 +0900

    GPT-4o-mini 모델 변경 및 코드 수정

--- TripPamineBE/src/main/java/com/example/trippaminebe/domain/mysterytour/service/MysteryTourAiService.java
+++ TripPamineBE/src/main/java/com/example/trippaminebe/domain/mysterytour/service/MysteryTourAiService.java
@@ -90,7 +90,7 @@ public class MysteryTourAiService {
 
         ResponseCreateParams params =
                 ResponseCreateParams.builder()
-                        .model(ChatModel.GPT_5_2)
+                        .model(ChatModel.GPT_4O_MINI)
                         .input(prompt)
                         .build();
 
